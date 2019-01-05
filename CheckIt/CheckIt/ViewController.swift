b89238e3d941fefa4756910180b54f75be25113e
Spencer Cawley <Spent_sir@Spencers-MacBook-Pro-2.local>
2019-01-04 23:17:12 -0700

    Added xcode project to github repo

---
+++ CheckIt/CheckIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckIt
+//
+//  Created by Spencer Cawley on 1/4/19.
+//  Copyright © 2019 Spencer Cawley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
